4c02672784801efc72e85ecf0fee1bd89f3c9589
pete <pete.dymarchuk@gmail.com>
2018-02-27 15:54:46 +0300

    clear

--- init.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.email "119784d4@mozej.com"      
-git config --global user.name "t2sae93o"
-
-sudo /etc/init.d/nginx start
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-
