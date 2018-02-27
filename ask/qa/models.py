e02134f4312c65933ca0f97e893c2cc121d31dc6
pete <pete.dymarchuk@gmail.com>
2018-02-27 20:59:42 +0300

    models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class QuestionManager(models.Manager):                                          
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
    title = models.CharField(max_length=255)
    text = models.TextField()
@@ -19,9 +28,3 @@ class Answer(models.Model):
    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING(None, None, None, None))
    author = models.ForeignKey(User, related_name='answer_author', on_delete=models.DO_NOTHING(None, None, None, None))
 
-class QuestionManager(models.Manager):                                          
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
