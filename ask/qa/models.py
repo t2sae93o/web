9759bde566844c6b5ff6aaf677aad4dc01cc5b20
pete <pete.dymarchuk@gmail.com>
2018-02-27 20:58:02 +0300

    models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,7 +13,15 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
+   objects = QuestionManager() 
    text = models.TextField()
    added_at = models.DateTimeField(auto_now=True)
    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING(None, None, None, None))
    author = models.ForeignKey(User, related_name='answer_author', on_delete=models.DO_NOTHING(None, None, None, None))
+
+class QuestionManager(models.Manager):                                          
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
