fc9f55273f7b31ac4124d0ccc598309a4bc26684
pete <pete.dymarchuk@gmail.com>
2018-02-28 12:35:02 +0300

    templates

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,11 +19,5 @@ from django.contrib.auth.views import login
 from qa.views import test
 
 urlpatterns = [  
-    url(r'^$', test),
-    url(r'^login/.*$', test),
-    url(r'^signup/.*$', test),
-    url(r'^question/\d+/$', test),
-    url(r'^ask/.*$', test),
-    url(r'^popular/.*$',test),
-    url(r'^new/.*$', test)
+   url(r'^', include('qa.urls')) 
 ]
